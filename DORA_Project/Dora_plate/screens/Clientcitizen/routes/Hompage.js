4f825ae8fb11f6151f6e002cb6432d02d5e3b320
robert <robertndive29@gmail.com>
2024-05-24 02:51:27 +0100

    changes

--- DORA_Project/Dora_plate/screens/Clientcitizen/routes/Hompage.js
+++ DORA_Project/Dora_plate/screens/Clientcitizen/routes/Hompage.js
@@ -13,13 +13,32 @@ import {
 const Chomepage = () => {
     return(
     <View style={styles.Container1}><Text style={styles.Text}>Welcome</Text>
+
+    <View style={styles.Container1}><Text style={styles.Text}>Welcome</Text></View>
+    <View style={styles.container1}><Text style={styles.Text}>Welcome</Text></View>
     </View>
+
     )
 }
 
 const styles = StyleSheet.create({
+    container1:{
+
+        backgroundColor:"blue"
+    },
+
+    container2:{
+        flex:2,
+ backgroundColor:"green"
+    },
+
+    container3:{
+        flex:1,
+        backgroundColor:"red"
+    },
+
     Container1: {
-      flex: 2,
+
       backgroundColor: "red",
       marginTop: 20,
       marginBottom: 120,
@@ -29,12 +48,21 @@ const styles = StyleSheet.create({
       display: "flex",
       fontWeight: "bolder",
     },
+    display:{backgroundColor:"green" },
+    display1:{backgroundColor:"green"}, 
+    display2:{backgroundColor:"blue"},
   Text: {
       color: "white",
       fontSize:60,
       textAlign:"center",
       justifyContent:"center"
     },
+    Text1: {
+        color: "white",
+        fontSize:20,
+        textAlign:"center",
+        justifyContent:"center"
+      },
    
   });
 
